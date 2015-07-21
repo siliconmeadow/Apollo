a4c76a4d3f5c75665b1b97d5d3f1995c7e1339bc
Cam Parry <cam.parry@gmail.com>
2015-07-21 23:38:08 +0100

    Fix kernel upgrade script to actually upgrade kernel in the right place

--- packer/scripts/ubuntu/base.sh
+++ packer/scripts/ubuntu/base.sh
@@ -2,15 +2,9 @@
 set -eux
 set -o pipefail
 
-# The sleep 30 in the example above is very important.
-# Because Packer is able to detect and SSH into the instance as soon as SSH is
-# available, Ubuntu actually doesn't get proper amounts of time to initialize.
-# The sleep makes sure that the OS properly initializes.
-sleep 30
-sudo apt-get upgrade -y
-sudo apt-get update -y
+echo `uname -r`
 
-sudo apt-get -y install git curl libcurl3 bridge-utils bundler default-jre-headless unzip wget python-setuptools python-protobuf cgroup-bin ruby1.9.1
+sudo apt-get -y install git curl libcurl3 bridge-utils bundler default-jre-headless unzip wget python-setuptools python-protobuf cgroup-bin ruby1.9.1 fail2ban
 sudo easy_install pip
 
 sudo pip install docker-py==1.1.0
--- packer/scripts/ubuntu/kernel-upgrade.sh
+++ packer/scripts/ubuntu/kernel-upgrade.sh
@@ -2,81 +2,17 @@
 set -eux
 set -o pipefail
 
-# Latest Available Kernel version
-LatestKernel="4.0.0-040000-generic"
+unset UCF_FORCE_CONFFOLD
+export UCF_FORCE_CONFFNEW=YES
+sudo ucf --purge /boot/grub/menu.lst
 
-# Required Packages
-Headers_All="http://kernel.ubuntu.com/~kernel-ppa/mainline/v3.19.8-vivid/linux-headers-3.19.8-031908_3.19.8-031908.201505110938_all.deb"
-Headers_i386="http://kernel.ubuntu.com/~kernel-ppa/mainline/v3.19.8-vivid/linux-headers-3.19.8-031908-generic_3.19.8-031908.201505110938_i386.deb"
-Image_i386="http://kernel.ubuntu.com/~kernel-ppa/mainline/v3.19.8-vivid/linux-image-3.19.8-031908-generic_3.19.8-031908.201505110938_i386.deb"
-Headers_amd64="http://kernel.ubuntu.com/~kernel-ppa/mainline/v3.19.8-vivid/linux-headers-3.19.8-031908-generic_3.19.8-031908.201505110938_amd64.deb"
-Image_amd64="http://kernel.ubuntu.com/~kernel-ppa/mainline/v3.19.8-vivid/linux-image-3.19.8-031908-generic_3.19.8-031908.201505110938_amd64.deb"
+sudo apt-get -y update
+sudo apt-get clean all
+sudo DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::="--force-confnew" install -fy linux-generic-lts-vivid linux-image-generic-lts-vivid linux-image-extra-3.19.0-22-generic
 
-# Debian Packages
-DEB="linux-headers-3.19.8*.deb linux-image-3.19.8*.deb"
+sudo update-grub2
 
-# Currently Installed Kernel Version
-CurrentKernel=$(uname -r) 
+sudo DEBIAN_FRONTEND=noninteractive dpkg -l 'linux-*' | sed '/^ii/!d;/'"$(uname -r | sed "s/\(.*\)-\([^0-9]\+\)/\1/")"'/d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d' | xargs sudo apt-get -y purge
 
-# System Architecture
-SystemArch=$(uname -i)
-
-# Check if System already has latest kernel installed
-if [ "$CurrentKernel" = "$LatestKernel" ]
-then
-  echo "Wowsers! Your System is Already Updated to Latest Available Kernel Version!"
-  echo "Program will now exit..."
-  sleep 2s
-  exit
-fi
-
-# If latest kernel is not available, then check the system architecture and download necessary packages
-
-# For 32-bit Systems
-
-if [ "$SystemArch" = "i386" ] || [ "$SystemArch" = "i686" ]
-then
-
-  echo "Kernel upgrade process for 32-bit systems will now start..."
-  sleep 2s
-  echo "Downloading required packages.."
-  sleep 2s
-
-  wget $Headers_All
-  wget $Headers_i386
-  wget $Image_i386
-
-  echo "Download process completed. Packages are present in $(pwd) directory"
-  sleep 2s
-
-  echo "Installing the packages..."
-  dpkg -i "$DEB"
-
-# For 64-bit Systems
-elif [ "$SystemArch" = "x86_64" ]
-then
-  echo "Kernel upgrade process for 64-bit systems will now start..." 
-  sleep 2s
-
-  wget $Headers_All
-  wget $Headers_amd64
-  wget $Image_amd64
-
-  echo "Download process completed. Packages are present in $(pwd) directory"
-  sleep 2s
-
-  echo "Installing the packages..."
-  dpkg -i "$DEB"
-
-# If system architecture is not compatible
-else
-  echo "Packages for following system architecture not found :  $SystemArch"
-  echo "Program will now exit..."
-  sleep 2s
-  exit
-fi
-
-echo "Your system has been successfully upgraded to latest kernel version $(LatestKernel)."
-echo "System will now reboot."
-reboot
+sudo reboot
 sleep 60
